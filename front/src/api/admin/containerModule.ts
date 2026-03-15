8e3e5dc5405521f27e0e3176a69268550c194415
Minh Cat DO <m.do@myskolae.fr>
2026-03-15 18:20:12 +0100

    refactor + feat: move accountCountStats useQuery hook to hooks/ + getContainerCountStats to show in adminHome

--- front/src/api/admin/containerModule.ts
+++ front/src/api/admin/containerModule.ts
@@ -1,12 +1,17 @@
 import { api } from "../axios";
 import { ENDPOINTS } from "../endpoints";
 export interface Container {
-id: number;
-created_at: string;
-city_name: string;
-postal_code: string;
-status: 'ready' | 'occupied' | 'maintenance';
-is_deleted: boolean;
+  id: number;
+  created_at: string;
+  city_name: string;
+  postal_code: string;
+  status: "ready" | "occupied" | "maintenance";
+  is_deleted: boolean;
+}
+
+export interface ContainerCountStats {
+  active: number;
+  total: number;
 }
 
 export const getAllContainers = async (): Promise<Container[]> => {
@@ -20,7 +25,9 @@ export const createContainer = async (container: Partial<Container>) => {
 };
 
 export const updateContainerStatus = async (id: number, status: string) => {
-  const response = await api.put(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`, { status });
+  const response = await api.put(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`, {
+    status,
+  });
   return response.data;
 };
 
@@ -33,3 +40,9 @@ export const getContainerDetails = async (id: number): Promise<Container> => {
   const response = await api.get(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`);
   return response.data;
 };
+
+export const getContainerCountStats =
+  async (): Promise<ContainerCountStats> => {
+    const response = await api.get(ENDPOINTS.ADMIN.CONTAINERS_COUNT);
+    return response.data;
+  };
