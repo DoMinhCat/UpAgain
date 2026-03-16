43a8526a6a1db27a9745c381fc0951dbb712db1f
Minh Cat DO <m.do@myskolae.fr>
2026-03-16 02:46:03 +0100

    feat: add useQuery hooks of events

---
+++ front/src/hooks/eventHooks.tsx
@@ -0,0 +1,28 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { showSuccessNotification } from "../components/NotificationToast";
+import {
+  getAllEvents,
+  getEventStats,
+  type EventStats,
+  type EventsListPagination,
+} from "../api/admin/eventModule";
+
+export const useGetAllEvents = (
+  page?: number,
+  limit?: number,
+  search?: string,
+  status?: string,
+  sort?: string,
+) => {
+  return useQuery<EventsListPagination>({
+    queryKey: ["events", page, limit, search, status, sort],
+    queryFn: () => getAllEvents(page, limit, search, status, sort),
+  });
+};
+
+export const useGetEventStats = () => {
+  return useQuery<EventStats>({
+    queryKey: ["eventStats"],
+    queryFn: getEventStats,
+  });
+};
