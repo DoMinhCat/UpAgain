10849939a6f34abed93d88fd9874c5d387154b7c
Minh Cat DO <m.do@myskolae.fr>
2026-03-19 06:43:51 +0100

    refactor(validation backend): reorganize code, dedup itemsQuery.go

--- back/db/depositQuery.go
+++ back/db/depositQuery.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// GenerateRandomCode generates a random 6-character code to open containers
 func GenerateRandomCode() string {
 	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	b := make([]byte, 6)
