734bf3ee40c5079938bac4b1d632cda359591f65
Minh Cat DO <m.do@myskolae.fr>
2026-03-21 16:27:14 +0100

    refactor: move interfaces of validation to interfaces/

---
+++ front/src/api/interfaces/listing.ts
@@ -0,0 +1,22 @@
+export interface Listing {
+  id_item: number;
+  title: string;
+  description: string;
+  material: string;
+  state: string;
+  weight: number;
+  price: number | null;
+  created_at: string;
+  city_name: string;
+  postal_code: string;
+  id_user: number;
+  username: string;
+}
+
+export interface PaginatedListingsResponse {
+  listings: Listing[];
+  current_page: number;
+  last_page: number;
+  limit: number;
+  total_records: number;
+}
