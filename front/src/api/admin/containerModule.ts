1dcb05ca9d4606e935471ab5070df3dc4185034a
Minh Cat DO <m.do@myskolae.fr>
2026-03-22 23:38:45 +0100

    fix: container endpoints

--- front/src/api/admin/containerModule.ts
+++ front/src/api/admin/containerModule.ts
@@ -16,19 +16,19 @@ export const createContainer = async (container: Partial<Container>) => {
 };
 
 export const updateContainerStatus = async (id: number, status: string) => {
-  const response = await api.put(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`, {
+  const response = await api.put(`${ENDPOINTS.ADMIN.CONTAINERS.ALL}${id}/`, {
     status,
   });
   return response.data;
 };
 
 export const deleteContainer = async (id: number) => {
-  const response = await api.delete(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`);
+  const response = await api.delete(`${ENDPOINTS.ADMIN.CONTAINERS.ALL}${id}/`);
   return response;
 };
 
 export const getContainerDetails = async (id: number): Promise<Container> => {
-  const response = await api.get(`${ENDPOINTS.ADMIN.CONTAINERS}${id}/`);
+  const response = await api.get(`${ENDPOINTS.ADMIN.CONTAINERS.ALL}${id}/`);
   return response.data;
 };
 
