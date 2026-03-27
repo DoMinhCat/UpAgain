00ea1fd55a7bee9bad8da1262962af72205abeeb
Minh Cat DO <m.do@myskolae.fr>
2026-03-27 16:51:11 +0100

    feat(frontend): start layout page detail post

--- front/src/pages/admin/postsAdmin/AdminPostDetails.tsx
+++ front/src/pages/admin/postsAdmin/AdminPostDetails.tsx
@@ -1,9 +1,25 @@
-import { Container } from "@mantine/core";
+import { Container, Title } from "@mantine/core";
+import { useLocation } from "react-router-dom";
+import AdminBreadcrumbs from "../../../components/admin/AdminBreadcrumbs";
 
 export const AdminPostDetails = () => {
+  const location = useLocation();
+  const origin = location.state || {};
   return (
-    <div>
-      <h1>Admin Post Details</h1>
-    </div>
+    <Container px="md" size="xl">
+      <Title order={2} mt="xs" mb="sm">
+        Post's Details
+      </Title>
+      <AdminBreadcrumbs
+        breadcrumbs={[
+          ...(origin.from === "allPosts"
+            ? [
+                { title: "Post Management", href: "/admin/posts" },
+                { title: "Post's Details", href: "/admin/posts/:id" },
+              ]
+            : []),
+        ]}
+      />
+    </Container>
   );
 };
