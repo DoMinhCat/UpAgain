60934e97487a24c24a20041e53331e144b9d3da5
Minh Cat DO <m.do@myskolae.fr>
2026-03-28 21:28:25 +0100

    refactor: hook of new syncing photo feature + dedicated carousel to show photos

--- front/src/hooks/postHooks.tsx
+++ front/src/hooks/postHooks.tsx
@@ -5,6 +5,7 @@ import {
   GetAllPosts,
   GetPostDetails,
   GetPostsStats,
+  UpdatePost,
 } from "../api/admin/postModule";
 import type { PostsListPagination } from "../api/interfaces/post";
 
@@ -79,6 +80,22 @@ export const useGetPostDetails = (id_post: number, isValidId: boolean) => {
     meta: {
       errorTitle: "Error fetching post details",
       errorMessage: "Failed to fetch post details",
+    },
+  });
+};
+
+export const useUpdatePost = (id_post: number) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (payload: FormData) => UpdatePost(id_post, payload),
+    meta: {
+      errorTitle: "Error updating post",
+      errorMessage: "Failed to update post",
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["postStats"] });
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      queryClient.invalidateQueries({ queryKey: ["postDetails", id_post] });
     },
   });
 };
