6b9f592d0540bd097dcb3996df840a261e20a3df
Minh Cat DO <m.do@myskolae.fr>
2026-03-28 21:57:54 +0100

    style: add title for container detail page

--- front/src/pages/admin/postsAdmin/AdminPostDetails.tsx
+++ front/src/pages/admin/postsAdmin/AdminPostDetails.tsx
@@ -199,7 +199,10 @@ export const AdminPostDetails = () => {
                 { title: "Post Management", href: "/admin/posts" },
                 { title: "Post's Details", href: "/admin/posts/:id" },
               ]
-            : []),
+            : [
+                { title: "Post Management", href: "/admin/posts" },
+                { title: "Post's Details", href: "/admin/posts/:id" },
+              ]),
         ]}
       />
 
