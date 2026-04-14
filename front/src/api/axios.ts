202b7873360d64e7cfd31fa424a88bb47f8b5f92
Ayoub241 <lamzayoub@gmail.com>
2026-04-14 20:35:39 +0200

    fix: .env

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -3,7 +3,12 @@ import { getNewAccessToken } from "./auth";
 import { ENDPOINTS } from "./endpoints";
 import { router } from "../routes/routes";
 
-const baseURL = "/api"; 
+let baseURL;
+if (import.meta.env.VITE_ENV === "DEV") {
+  baseURL = import.meta.env.VITE_API_BASE_URL_DEV;
+} else {
+  baseURL = import.meta.env.VITE_API_BASE_URL_PROD;
+}
 
 export const api = axios.create({
   baseURL: baseURL,
