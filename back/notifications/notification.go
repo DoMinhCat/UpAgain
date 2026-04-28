3fee25cb3095a6370ded991e4fc350fb69c5a0ed
Minh Cat DO <m.do@myskolae.fr>
2026-04-28 23:09:29 +0200

    feat: helper function to send noti, save noti to db

--- back/notifications/notification.go
+++ back/notifications/notification.go
@@ -1,10 +1,59 @@
 package notifications
 
+import (
+	"backend/utils"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 const (
 	ONE_SIGNAL_API_BASE_URL = "https://api.onesignal.com/notifications?c=push"
 )
 
-// Send a POST request to OneSignal API
-func SendNotification(payload NotificationRequest) {
+// Send a POST request to OneSignal API to push notification to client
+//
+// Should log error only and not block the whole operation/request
+func SendNotification(payload NotificationRequest) error {
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("SendNotification() JSON marshaling failed: %w", err)
+	}
 
+	// HTTP request to OneSignal API
+	req, err := http.NewRequest("POST", ONE_SIGNAL_API_BASE_URL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("SendNotification() HTTP request failed: %w", err)
+	}
+
+	// Set Headers
+	apiKey := utils.GetOnesignalRestApiKey()
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Key " + apiKey)
+
+	// Execute Request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("SendNotification() HTTP request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("SendNotification() HTTP request failed: %d", resp.StatusCode)
+	}
+	
+	body, _ := io.ReadAll(resp.Body)
+
+	var result OneSignalResponse
+    if err := json.Unmarshal(body, &result); err != nil {
+        return fmt.Errorf("SendNotification() failed to parse OneSignal response: %s", string(body))
+    }
+
+    if result.Errors != nil {
+        return fmt.Errorf("SendNotification() errors returned from OneSignal: %v", result.Errors)
+    }
+
+	return nil
 }
