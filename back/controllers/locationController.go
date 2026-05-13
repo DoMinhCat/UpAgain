d5153488c753706434d7596e8b5bf7abcde0efb7
Do Minh Cat <m.do@myskolae.fr>
2026-05-13 18:40:46 +0200

    feat(backend): controller GetCoorFromAddress done

--- back/controllers/locationController.go
+++ back/controllers/locationController.go
@@ -2,35 +2,59 @@ package controllers
 
 import (
 	"backend/models"
-	"backend/utils/location"
+	"backend/utils"
+	"backend/utils/geocode"
 	"net/http"
 )
 
 func GetAddressFromCoor(w http.ResponseWriter, r *http.Request) {
 	// get params: lat, lng
 
-	var coordinates models.Coordinates
-	// bind params to coordinates
+	var params models.Coordinates
+	// bind params
 
-	// Call to utils 
-	address, err := location.CoorToAddress(coordinates)
+	// validate
+	if params.Lat > 90 || params.Lat < -90 {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid latitude")
+		return
+	}
+	if params.Lng > 90 || params.Lng < -90 {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid longitude")
+		return
+	}
+
+	address, err := geocode.CoorToAddress(params)
 	if err != nil{
 
 	}
-
+	utils.RespondWithJSON(w, http.StatusOK, address)
 }
 
 func GetCoorFromAddress(w http.ResponseWriter, r *http.Request) {
 	// get params: street, postal_code, city
-	var address models.Address
-	// bind params to address
 
+	query := r.URL.Query()
+	street := query.Get("street")
+	postal_code := query.Get("postal_code")
+	city := query.Get("city")
 
-
-	// Call to utils 
-	coordinates, err := location.AddressToCoor(address)
-	if err != nil{
-		
+	// validate
+	if street == "" || postal_code == "" || city == ""{
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid address")
+		return
 	}
 
+	params := models.Address{
+		Street: street,
+		PostalCode: postal_code,
+		City: city,
+	}
+
+	coordinates, err := geocode.AddressToCoor(params)
+	if err != nil{
+		utils.RespondWithError(w, http.StatusInternalServerError, "Failed to retrieve coordinates from given address")
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, coordinates)
 }
