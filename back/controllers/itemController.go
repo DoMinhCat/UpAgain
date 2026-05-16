c16cb6eee64905e60d0b3a0f8779fb60f00d37e5
Minh Cat DO <m.do@myskolae.fr>
2026-05-17 01:27:04 +0200

    feat: add filter by cancelled status

--- back/controllers/itemController.go
+++ back/controllers/itemController.go
@@ -902,7 +902,7 @@ func CancelItemReservation(w http.ResponseWriter, r *http.Request) {
 	// get uuid
 	uuid, err := db.GetTransactionLatestUuidOfPro(idRequestor, item_id)
 	if err != nil {
-		slog.Error("GetTransactionLatestUUIDByItemId() failed", "controller", "CancelItemReservation", "error", err)
+		slog.Error("GetTransactionLatestUuidOfPro() failed", "controller", "CancelItemReservation", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "An error occurred while cancelling item.")
 		return
 	}
