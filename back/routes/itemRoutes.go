b6cfdcd3f79a8c3134daf00f95dd37826cd94501
Minh Cat DO <m.do@myskolae.fr>
2026-05-20 00:35:53 +0200

    feat(backend): init confirm code endpoint

--- back/routes/itemRoutes.go
+++ back/routes/itemRoutes.go
@@ -14,10 +14,10 @@ func GetItemRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /items/{item_id}/transactions/latest/{$}", middleware.AuthMiddleware([]string{"pro", "user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetLatestTransaction))))
 	mux.Handle("GET /items/me/{$}", middleware.AuthMiddleware([]string{"user", "pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetMyItems))))
 
-
 	mux.Handle("POST /items/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.CreateItem))))
 	mux.Handle("POST /items/{item_id}/reserve/{$}", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ReserveItem))))
 	mux.Handle("POST /items/{item_id}/purchase/{$}", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.PurchaseItem))))
+	mux.Handle("POST /items/{item_id}/confirm/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ConfirmListingRetrieval))))
 
 	mux.Handle("PATCH /items/{item_id}/{$}", middleware.AuthMiddleware([]string{"admin", "user", "pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.UpdateItemStatusById))))
 
