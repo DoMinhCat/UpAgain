8b5ec14f4dcedbefb94053a09f55bc6d4e48d964
Arnaud <alesausse@myges.fr>
2026-05-21 10:54:00 +0000

    fix: global routing cleanup to avoid 307 redirects and fix corrupted comments

--- back/routes/depositRoutes.go
+++ back/routes/depositRoutes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetDepositRoutes(mux *http.ServeMux) {
-	mux.Handle("GET /deposits/{deposit_id}/{$}", middleware.AuthMiddleware([]string{"user", "pro", "admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetDepositDetailsById))))
-	mux.Handle("GET /deposits/{deposit_id}/codes/{$}", middleware.AuthMiddleware([]string{"user", "pro", "admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetDepositCodesOfLatestTransactionByDepositId))))
-	mux.Handle("POST /deposits/{deposit_id}/transfer/{$}", middleware.AuthMiddleware([]string{"user", "pro", "admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.TransferContainerByDepositId))))
+	mux.Handle("GET /deposits/{deposit_id}", middleware.AuthMiddleware([]string{"user","pro","admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetDepositDetailsById))))
+	mux.Handle("GET /deposits/{deposit_id}/codes", middleware.AuthMiddleware([]string{"user","pro","admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetDepositCodesOfLatestTransactionByDepositId))))
+	mux.Handle("POST /deposits/{deposit_id}/transfer", middleware.AuthMiddleware([]string{"user","pro","admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.TransferContainerByDepositId))))
 }
