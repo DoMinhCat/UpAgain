05f7bf9ccd1e6920b2857a7ecaac4a7eea6ff844
Arnaud <alesausse@myges.fr>
2026-05-21 05:08:38 +0000

    chore: merge conflicts

--- back/routes/itemRoutes.go
+++ back/routes/itemRoutes.go
@@ -7,14 +7,13 @@ import (
 )
 
 func GetItemRoutes(mux *http.ServeMux) {
-	mux.Handle("GET /items/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItems))))
-	mux.Handle("GET /items/count/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemsCount))))
-	mux.Handle("GET /items/{id}/{$}", middleware.AuthMiddleware([]string{"admin", "user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemById))))
-	mux.Handle("DELETE /items/{id}/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.DeleteItem))))
-	mux.Handle("POST /items/{id}/reserve/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ReserveItem))))
-	mux.Handle("POST /items/{id}/cancel/{$}", middleware.AuthMiddleware([]string{"user", "admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.CancelReservation))))
-	mux.Handle("POST /items/{id}/purchase/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.PurchaseItem))))
-	mux.Handle("GET /items/{id}/transactions/latest/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetLatestTransaction))))
-	mux.Handle("GET /items/{id}/transactions/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemTransactions))))
-	mux.Handle("POST /items/{id}/transactions/{uuid}/cancel/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.CancelTransaction))))
+	mux.Handle("GET /items/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetAllItems))))
+	mux.Handle("GET /items/count/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetAllItemsStats))))
+	mux.Handle("GET /items/{item_id}/{$}", middleware.AuthMiddleware([]string{"admin", "user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemDetails))))
+	mux.Handle("DELETE /items/{item_id}/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.DeleteItemById))))
+	mux.Handle("POST /items/{item_id}/reserve/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ReserveItem))))
+	mux.Handle("POST /items/{item_id}/cancel/{$}", middleware.AuthMiddleware([]string{"user", "admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.CancelItemReservation))))
+	mux.Handle("POST /items/{item_id}/purchase/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.PurchaseItem))))
+	mux.Handle("GET /items/{item_id}/transactions/latest/{$}", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetLatestTransaction))))
+	mux.Handle("GET /items/{item_id}/transactions/{$}", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemTransactions))))
 }
