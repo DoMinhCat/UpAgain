ddae9f5067185616ed515c8d9e2431da63a819e1
Minh Cat DO <m.do@myskolae.fr>
2026-05-25 01:14:03 +0200

    fix: container route trailing slash and permission

--- back/routes/itemRoutes.go
+++ back/routes/itemRoutes.go
@@ -19,7 +19,7 @@ func GetItemRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /items/count", middleware.AuthMiddleware([]string{"admin"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetAllItemsStats))))
 	mux.Handle("GET /items/{item_id}", middleware.AuthMiddleware([]string{"admin", "user", "pro", "employee"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemDetails))))
 	mux.Handle("GET /items/{item_id}/transactions", middleware.AuthMiddleware([]string{"admin", "user", "pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetItemTransactions))))
-	mux.Handle("GET /items/{item_id}/transactions/latest", middleware.AuthMiddleware([]string{"user"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetLatestTransaction))))
+	mux.Handle("GET /items/{item_id}/transactions/latest", middleware.AuthMiddleware([]string{"user", "pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.GetLatestTransaction))))
 
 	mux.Handle("PATCH /items/{item_id}", middleware.AuthMiddleware([]string{"admin", "user", "pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.UpdateItemStatusById))))
 
