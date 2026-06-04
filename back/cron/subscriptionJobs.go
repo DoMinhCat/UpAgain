68ecae1084707bf6c563e7aa65c6d07e36742b4e
Minh Cat DO <m.do@myskolae.fr>
2026-06-04 16:10:59 +0200

    feat(backend): UpdateExpiredSubscription cron job

--- back/cron/subscriptionJobs.go
+++ back/cron/subscriptionJobs.go
@@ -1,1 +1,39 @@
 package cron
+
+import (
+	"backend/utils"
+	"log/slog"
+	"time"
+)
+
+func UpdateExpiredSubscription() {
+    slog.Info("UpdateExpiredSubscription started", "time", time.Now().Format(time.RFC3339))
+
+	query := `
+	UPDATE pros p
+	SET is_premium = false
+	FROM subscriptions s
+	WHERE s.id_pro = p.id_account
+		AND s.is_active = true
+		AND s.sub_to < NOW();
+	`
+	_, err := utils.Conn.Exec(query)
+	if err != nil {
+		slog.Error("UpdateExpiredSubscription failed", "error", err)
+		return
+	}
+
+    query = `
+    UPDATE subscriptions
+    SET is_active = false
+    WHERE is_active = true
+        AND sub_to < NOW();
+    `
+    _, err = utils.Conn.Exec(query)
+    if err != nil {
+        slog.Error("UpdateExpiredSubscription failed", "error", err)
+        return
+    }
+    
+    slog.Info("UpdateExpiredSubscription completed")
+}
