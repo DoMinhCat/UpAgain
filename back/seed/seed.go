3d016918bc278630d6c0125697a50fe872c8e0c9
Do Minh Cat <m.do@myskolae.fr>
2026-06-16 15:07:39 +0200

    refactor: one tx for whole seeding process

--- back/seed/seed.go
+++ back/seed/seed.go
@@ -2,6 +2,7 @@ package seed
 
 import (
 	"backend/seed/utils"
+	"backend/seed/utils/insert"
 	"fmt"
 )
 
@@ -17,12 +18,29 @@ func SeedDB() {
 	fmt.Println("Seed DB connected")
 	defer conn.Close()
 
+	tx, err := conn.Begin()
+	if err != nil {
+		panic(fmt.Sprintf("failed to start transaction: %v", err))
+	}
+	defer tx.Rollback()
+
 	// Clean databse for fresh restart and reset id auto-increment
-	utils.CleanSeedDb(conn)
+	err = utils.CleanSeedDb(tx)
+	if err != nil {
+		panic(fmt.Sprintf("CleanSeedDb failed: %v", err))
+	}
 
 	// Insert data (modular functions to avoid messy code)
 	// TODO: call modular functions to insert into table 1 by 1
+	err = insert.InsertAccounts(tx)
+	if err != nil {
+		panic(fmt.Sprintf("InsertAccounts failed: %v", err))
+	}
 
+	err = tx.Commit()
+	if err != nil {
+		panic(fmt.Sprintf("failed to commit transaction: %v", err))
+	}
 	fmt.Println("Seeding completed successfully!")
 }
 
