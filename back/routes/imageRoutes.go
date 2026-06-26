32c962eed439ed5b508448b3970fc6110b042812
Minh Cat DO <m.do@myskolae.fr>
2026-06-26 02:47:11 +0200

    fix: add web endpoint for serving image

--- back/routes/imageRoutes.go
+++ back/routes/imageRoutes.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
-	"backend/controllers"
-	"backend/middleware"
+	// "backend/controllers"
+	// "backend/middleware"
 	"net/http"
 )
 
 func GetImageRoutes(mux *http.ServeMux) {
-	mux.Handle("GET /images", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ServeImageHandler))))
+	// to serve mobile app
+	// mux.Handle("GET /images", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ServeImageHandler))))
+
+	// for serving web images directly from disk
+	mux.Handle("GET /images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 }
