78a01f6c5aef9c3decc2e7c19430eb667834eb28
Minh Cat DO <m.do@myskolae.fr>
2026-06-26 03:28:49 +0200

    hotfix: re-enable mobile image endpoint

--- back/routes/imageRoutes.go
+++ back/routes/imageRoutes.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	// "backend/controllers"
-	// "backend/middleware"
+	"backend/controllers"
+	"backend/middleware"
 	"net/http"
 )
 
 func GetImageRoutes(mux *http.ServeMux) {
 	// to serve mobile app
-	// mux.Handle("GET /images", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ServeImageHandler))))
+	mux.Handle("GET /mobile/images", middleware.AuthMiddleware([]string{"pro"}, middleware.UpdateLastActive(http.HandlerFunc(controllers.ServeImageHandler))))
 
 	// for serving web images directly from disk
 	mux.Handle("GET /images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
