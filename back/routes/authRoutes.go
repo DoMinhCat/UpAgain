73516e746150e8f6fc192b66fec339796096691e
Ayoub241 <lamzayoub@gmail.com>
2026-07-07 16:21:11 +0200

    feat: add GET /verify-email/{token}/ endpoint

--- back/routes/authRoutes.go
+++ back/routes/authRoutes.go
@@ -8,4 +8,5 @@ import (
 func GetAuthRoutes(mux *http.ServeMux) {
 	mux.Handle("POST /login", http.HandlerFunc(controllers.Login))
 	mux.Handle("POST /refresh", http.HandlerFunc(controllers.RefreshToken))
+	mux.Handle("GET /verify-email/{token}/", http.HandlerFunc(controllers.VerifyEmail))
 }
